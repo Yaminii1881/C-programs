e8d41258617bde4600e3abb2de53e4933f92a975
Yamini <syamy1488@gmail.com>
2026-09-12 13:03:21 +0530

    Create Day1.C

---
+++ Day1.C
@@ -0,0 +1,166 @@
+              TODAY PROGRAMS
+Program1:Print Hello World 
+  #include<stdio.h>
+   int main() 
+  {
+   printf("Hello world") ;
+   return 0;
+} 
+  OUTPUT: Hello World
+----------------------------------------------
+Program2:Print about yourself
+  #include<stdio.h>
+  int main() 
+{
+  printf("My name is Yamini\n") ;
+  printf("I am btech 1st year student\n") ;
+  printf("My goal is to work at Google") ;
+  return0;
+} 
+  OUTPUT:My name is Yamini
+         I am btech 1st year student 
+         My goal is to work at Google
+--------------------—--------------------------
+Program3:Print Addition of two numbers 
+ #includd<stdio.h>
+ int main() 
+{
+ int a, b, sum;
+ printf("enter a, b values:") ;
+ scanf("%d %d", &a,&b) ;
+ sum=a+b;
+ printf("Addition of two numbers:%d", sum) ;
+ return 0 ;
+} 
+  OUTPUT:enter a, b values:5 6
+         Addition of two numbers:11
+-----------------------------------------------
+Program4:Subtraction of two numbers
+ #include<studio.h>
+ int main() 
+{
+ int a=7,b=5;
+ int sub=a-b;
+ printf("sub of two numbers :%d", sub) ;
+ return 0 ;
+} 
+OUTPUT:sub of two numbers :2
+--------------------------------------------
+Program5:Multiplication of two numbers 
+ #include<studio.h>
+  int main() 
+ {
+  int a=3,b=4;
+  int mul=a*b;
+  printf("Multiplication of two numbers:%d", mil) ;
+return 0 ;
+} 
+OUTPUT:Multiplication of two numbers:12
+--------—--------------------------------------
+Program6:Division of two numbers 
+ #include <studio.h>
+  int main () 
+{
+  int a=6,b=3;
+  int div=a/b;
+  printf("Division of two numbers:%d", do) ;
+  return 0;
+} 
+OUTPUT:Division of two numbers:2
+----------------------------------------------
+Program7:Modular division of two numbers 
+#include <stdio.h.>
+ int main () 
+{
+  int a=9,b=2;
+  int mod=a%b;
+  printf("Modular division of two numbers:%d", mod) 
+  return 0;
+} 
+OUTPUT:Modular division of two numbers:1
+-----------------------------------------------
+Program8:Area of circle
+ #include<studio.h>
+ int main() 
+{
+ int r=2;
+ float pi=3.14;
+ float area=pi*r*r;
+ printf("Area of circle:%f", area) ;
+ return 0 ;
+} 
+OUTPUT: Area of circle:12.56
+-----------------------------------------------Program9:Area of Rectangle 
+ #include<stdio.h>
+ int main () 
+{
+ int l, b, area;
+ printf("enter l, b values:") ;
+ scanf("%d %d", &l,&b) ;
+ int area=l*b;
+ printf("Area of Rectangle:%d", area) ;
+ return 0 ;
+} 
+OUTPUT:enter l, b values:3,4
+       Area of Rectangle:12
+-----------------------------------------------Program10:Perimeter of Rectangle 
+ #include<studio.h>
+ int main () 
+{
+ int a=3,b=4;
+ int perimeter=2(a+b);
+ printf("Perimeter of the Rectangule:%d", perimeter) ;
+ return 0 ;
+} 
+OUTPUT:Perimeter of the Rectangle:14
+-----------------------------------------------
+Program11:Average of three marks      
+ #include<stdio.h>
+  int main() 
+{
+  int a, b, c, avg;
+  printf("enter a, b, c values:") ;
+  scanf("%d %d %d", &a,&b,&c);
+  avg=a+b+c/ 3;
+  printf("Average of three marks:%d", avg) ;
+  return 0 ;
+} 
+OUTPUT:enter a, b, c values:3 4 5
+       Average of three marks:4
+-----------------------------------------------
+PROGRAM12:Swap of two numbers 
+ #include<stdio.h>
+  int main() 
+ {
+  int a, b, temp;
+  printf("enter a, b values:") ;
+  scanf("%d %d", &a, &b) ;
+  temp =a;
+  a=b;
+  temp=b;
+  printf("After swapping\n") ;
+  printf("value of a is %d", a); 
+  printf("value of b is %d", b);
+  return 0 ;
+} 
+OUTPUT:enter a, b values:5 4
+       value of a is 4
+       value of b is 5
+-----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
